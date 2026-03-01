b42e56f4159b72a46083e47e02531312e18118d0
SmurfEow <tiramisu026@gmail.com>
2026-03-01 23:09:23 +0800

    SSL update

--- src/Server/ServerSocket.java
+++ src/Server/ServerSocket.java
@@ -1,27 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Server;
+
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import javax.rmi.ssl.SslRMIClientSocketFactory;
 import javax.rmi.ssl.SslRMIServerSocketFactory;
-import java.rmi.Naming;
 
 public class ServerSocket {
     public static void main(String[] args) {
         try {
-            // ✅ SSL registry
-            Registry reg = LocateRegistry.createRegistry(
-                1099,
-                new SslRMIClientSocketFactory(),
-                new SslRMIServerSocketFactory()
-            );
+            // Force hostname so client gets correct stub (PC A IP)
+            System.setProperty("java.rmi.server.hostname", "192.168.1.19");
 
-            // ✅ bind service (ServiceImpl must also be SSL-exported, see next section)
+            var csf = new SslRMIClientSocketFactory();
+            var ssf = new SslRMIServerSocketFactory();
+
+            // 1) SSL registry
+            Registry reg = LocateRegistry.createRegistry(1099, csf, ssf);
+
+            // 2) ServiceImpl is already SSL-exported (because it extends UnicastRemoteObject)
             ServiceImpl svc = new ServiceImpl();
-            Naming.rebind("rmi://0.0.0.0:1099/Authorization", svc);
+
+            // 3) Bind using registry (don’t use Naming + 0.0.0.0)
+            reg.rebind("Authorization", svc);
 
             System.out.println("✅ SSL-RMI Server started on port 1099");
         } catch (Exception e) {
